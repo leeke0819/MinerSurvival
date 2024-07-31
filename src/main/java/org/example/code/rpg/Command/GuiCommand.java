ba2f6ff45d36e56462d55e4b479cae68628701f9
leeke0819 <jaesuk819@gmail.com>
2024-07-31 21:06:58 +0900

    2024 / 07 / 31 (오후 9시)

--- src/main/java/org/example/code/rpg/Command/GuiCommand.java
+++ src/main/java/org/example/code/rpg/Command/GuiCommand.java
@@ -1,17 +1,11 @@
 package org.example.code.rpg.Command;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.example.code.rpg.Manager.GuiManager;
 import org.example.code.rpg.RPG;
 
 public class GuiCommand implements CommandExecutor {
@@ -28,66 +22,9 @@ public class GuiCommand implements CommandExecutor {
             return true;
         }
         Player player = (Player) sender;
-        openGui(player);
+        GuiManager guiManager = new GuiManager();
+        guiManager.openGui(player);
         return true;
     }
 
-    public void openGui(Player player) {
-        Inventory inventory = Bukkit.createInventory(null, 27, "메뉴");
-
-        ItemStack itemStack = new ItemStack(Material.WHITE_STAINED_GLASS_PANE, 1);
-        ItemStack itemStack1 = new ItemStack(Material.ENCHANTED_BOOK, 1);
-        ItemStack itemStack2 = new ItemStack(Material.NETHERITE_PICKAXE, 1);
-        ItemStack itemStack3 = new ItemStack(Material.PAPER, 1);
-        ItemStack itemStack4 = new ItemStack(Material.WRITABLE_BOOK, 1);
-
-        ItemMeta meta1 = itemStack1.getItemMeta();
-        ItemMeta meta2 = itemStack2.getItemMeta();
-        ItemMeta meta3 = itemStack3.getItemMeta();
-        ItemMeta meta4 = itemStack4.getItemMeta();
-
-        if (meta1 != null) {
-            meta1.setDisplayName(ChatColor.YELLOW.toString() + ChatColor.BOLD + "전직");
-            meta1.addItemFlags(ItemFlag.HIDE_ATTRIBUTES); // 태그 숨기기
-            itemStack1.setItemMeta(meta1);
-        }
-        if (meta2 != null) {
-            meta2.setDisplayName(ChatColor.GOLD.toString() + ChatColor.BOLD + "광물 판매");
-            meta2.addEnchant(Enchantment.DURABILITY, 1, true); // 인챈트 부여
-            meta2.addItemFlags(ItemFlag.HIDE_ENCHANTS); // 인챈트 숨기기
-            meta2.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-            itemStack2.setItemMeta(meta2);
-        }
-        if (meta3 != null) {
-            meta3.setDisplayName(ChatColor.GRAY.toString() + ChatColor.BOLD + "단서");
-            meta3.addEnchant(Enchantment.DURABILITY, 1, true); // 인챈트 부여
-            meta3.addItemFlags(ItemFlag.HIDE_ENCHANTS); // 인챈트 숨기기
-            meta3.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-            itemStack3.setItemMeta(meta3);
-        }
-        if (meta4 != null) {
-            meta4.setDisplayName(ChatColor.GREEN.toString() + ChatColor.BOLD + "도움말");
-            meta4.addEnchant(Enchantment.DURABILITY, 1, true); // 인챈트 부여
-            meta4.addItemFlags(ItemFlag.HIDE_ENCHANTS); // 인챈트 숨기기
-            meta4.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-            itemStack4.setItemMeta(meta4);
-        }
-
-
-        for(int i = 0; i < 9; i++) {
-            inventory.setItem(i, itemStack);
-        }
-        inventory.setItem(9, itemStack);
-
-        inventory.setItem(10, itemStack1);
-        inventory.setItem(12, itemStack2);
-        inventory.setItem(14, itemStack3);
-        inventory.setItem(16, itemStack4);
-
-        inventory.setItem(17, itemStack);
-        for(int i = 18; i < 27; i++) {
-            inventory.setItem(i, itemStack);
-        }
-        player.openInventory(inventory);
-    }
 }
--- src/main/java/org/example/code/rpg/Event/InventoryClickListener.java
+++ src/main/java/org/example/code/rpg/Event/InventoryClickListener.java
@@ -1,57 +1,38 @@
 package org.example.code.rpg.Event;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-
-import static org.bukkit.Bukkit.getLogger;
+import org.example.code.rpg.Manager.GuiManager;
 
 public class InventoryClickListener implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if(!event.getView().getTitle().equals("메뉴")) return;
+        if(!event.getView().getTitle().equals("메뉴") && !event.getView().getTitle().equals("전직 상점")) return;
         event.setCancelled(true);
         Player player = (Player) event.getWhoClicked();
         ItemStack clickedItem = event.getCurrentItem();
-        if (clickedItem == null || clickedItem.getType() == Material.AIR) return;
-        if (clickedItem != null && clickedItem.getType() == Material.ENCHANTED_BOOK) {
-            if (clickedItem.getItemMeta() != null && ChatColor.stripColor(clickedItem.getItemMeta().getDisplayName()).equals("전직")) {
-                event.setCancelled(true);
-                openNewGui1((InventoryClickEvent) player);
+        if (clickedItem != null){
+            event.setCancelled(true);
+            if (clickedItem.getType() == Material.ENCHANTED_BOOK){
+                if (clickedItem.getItemMeta() != null && ChatColor.stripColor(clickedItem.getItemMeta().getDisplayName()).equals("전직")) {
+                    GuiManager guiManager = new GuiManager();
+                    guiManager.openGui(player);
+                }
+                else if (clickedItem.getType() == Material.AIR) {
+                    return;
+                }
+                //여기서 elseif 계속 써가면서 커스텀아이템 검증하는 로직 작성
             }
+            event.setCancelled(true);
         }
         // 아이템 클릭에 따른 행동 정의
         if (clickedItem.getType() == Material.STONE) {
             player.sendMessage("You clicked on a Stone1!");
         }
-    }
-    @EventHandler
-    public void openNewGui1(InventoryClickEvent event) {
-        event.setCancelled(true);
-        Player player = (Player) event.getWhoClicked();
-        ItemStack clickedItem = event.getCurrentItem();
-        if (clickedItem == null || clickedItem.getType() == Material.AIR) return;
-
-        Inventory newInventory = Bukkit.createInventory(null, 45, "전직 상점");
-        ItemStack itemStack = new ItemStack(Material.WHITE_STAINED_GLASS_PANE, 1);
-        for (int i = 0; i < 9; i++) {
-            newInventory.setItem(i, itemStack);
-        }
-        newInventory.setItem(9, itemStack);
-        newInventory.setItem(17, itemStack);
-        newInventory.setItem(18, itemStack);
-        newInventory.setItem(26, itemStack);
-        newInventory.setItem(27, itemStack);
-        newInventory.setItem(35, itemStack);
-        for (int i = 36; i < 45; i++) {
-            newInventory.setItem(i, itemStack);
-        }
-        player.openInventory(newInventory);
     }
 }
